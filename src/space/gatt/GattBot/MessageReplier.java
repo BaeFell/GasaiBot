6b9573f538a9245c69c2010b2c9f0d473ac7a77a
Gatt <zacharygatt@me.com>
2016-03-16 22:13:41 +1100

    No more unflippin

--- src/space/gatt/GattBot/MessageReplier.java
+++ src/space/gatt/GattBot/MessageReplier.java
@@ -12,12 +12,6 @@ public class MessageReplier implements MessageCreateListener {
 
     @Override
     public void onMessageCreate(DiscordAPI discordAPI, Message message) {
-        if (message.getContent().contains("(╯°□°）╯︵ ┻━┻")){
-            MessageBuilder builder = new MessageBuilder();
-            builder.append("┬─┬\uFEFF ノ( ゜-゜ノ)");
-            message.reply(builder.build());
-            return;
-        }
         if (message.getMentions().contains(discordAPI.getYourself())){
             MessageBuilder builder = new MessageBuilder();
             builder.append(Settings.getMsgStarter()).appendUser(message.getAuthor()).append(" I'm GattBot! Use /help to get a list of all my commands!");
