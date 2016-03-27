d5a8504ecb7e6446b89ef965c6b9cd39576f266a
Gatt <zacharygatt@me.com>
2016-03-27 20:36:21 +1100

    Lets' see if this works for restarting.

--- src/space/gatt/GattBot/AdminCommands.java
+++ src/space/gatt/GattBot/AdminCommands.java
@@ -549,7 +549,7 @@ public class AdminCommands implements MessageCreateListener {
     private void reboot() throws Exception{
         ProcessBuilder builder = new ProcessBuilder();
         builder.redirectOutput(ProcessBuilder.Redirect.INHERIT);
-        builder.command("nohup java -jar /var/lib/jenkins/workspace/GattBot/target/GattBot-1.0-SNAPSHOT.jar " + Main.getEmail()+" " + Main.getPassword()+ " reboot &\n");
+        builder.command("startgattbot.sh\n");
         Process botProcess = builder.start();
         botWriter = new BufferedWriter(new OutputStreamWriter(botProcess.getOutputStream()));
         switch (botProcess.exitValue()) {
