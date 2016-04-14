e26fcd8449f0e1fbaad5d391d9bcfaf9b4f55a63
Gatt <zacharygatt@me.com>
2016-04-14 17:11:52 +1000

    Reverted changes

--- src/space/gatt/GattBot/Main.java
+++ src/space/gatt/GattBot/Main.java
@@ -81,7 +81,7 @@ public class Main {
 
         DiscordAPI botOwner = Javacord.getApi(email, password);
         botOwner.connectBlocking();
-        api = Javacord.getApi(token, true);
+        api = Javacord.getApi(gasaiemail, gasaipassword);
         api.connectBlocking();
         api.convertToBotAccount(botOwner.getToken());
 
